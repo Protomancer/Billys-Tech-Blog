ee3af89ac58b1c626aa7fdff49dc50ae4f5171c1
Billy Pinkerton <billybattlebones@gmail.com>
2022-07-04 13:46:18 -0700

    updating tech-blog

---
+++ models/Posts.js
@@ -0,0 +1,38 @@
+const {Module, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Posts extends Model {}
+
+Posts.init(
+    {
+        id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+        },
+        post_title: {
+        type: DataTypes.STRING,
+        allowNull: false
+        },
+        post_contents: {
+        type: DataTypes.STRING,
+        allowNull: false
+        },
+        users_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'users',
+            key:'id'
+            }
+        }  
+    },
+    {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'posts'
+    }
+);
+
+module.exports = Posts;
